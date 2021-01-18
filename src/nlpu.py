7367b48533c2113ea7f6d96feefdc0909bc9716c
Jack Demonakis <jackdemonakis@gmail.com>
2021-01-18 16:40:59 +0000

    Added date recognition to NLPU

--- src/nlpu.py
+++ src/nlpu.py
@@ -1,11 +1,18 @@
 import re
 import spacy
 import ticket_generator
+import datetime
 
 nlp = spacy.load("en_core_web_lg")
 
 def get_times(message):
     return [time[0] for time in re.findall("((([1-9])|([0-1][0-9])|(2[0-3])):(([0-5][0-9])))", message)]
+
+def get_dates(message):
+    dates = []
+    for date in re.findall(r"(\d+/\d+/\d+)", message):
+        dates.append(datetime.datetime.strptime(date, "%d/%m/%y"))
+    return dates
 
 def process_message(message):
     doc = nlp(message)
@@ -19,11 +26,18 @@ def process_message(message):
             from_station_name = str(ent).title()
         elif "to " + str(ent) in message:
             to_station_name = str(ent).title()
+    dates = get_dates(message)
     times = get_times(message)
+
     time = None
-    if len(times) > 0:
-        time = times[0]
-    
+    if len(dates) > 0:
+        time = dates[0]
+        timesplit = times[0].split(":")
+        time = time.replace(hour= int(timesplit[0]), minute= int(timesplit[1]))
+    elif len(times) > 0:
+        time = datetime.datetime.now()
+        timesplit = times[0].split(":")
+        time = time.replace(hour= int(timesplit[0]), minute= int(timesplit[1]))
     from_station = ticket_generator.Station.get_from_name(from_station_name)
     to_station = ticket_generator.Station.get_from_name(to_station_name)
 
@@ -33,3 +47,6 @@ def process_message(message):
         return "I need to know where you are coming from and where you want to go to."
 
     return str(ticket_generator.get_tickets(from_station, to_station))
+
+
+process_message("11/21 12:01")
