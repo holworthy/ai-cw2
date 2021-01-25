2f565add88e3f90e1114bca852ad143771033db1
Daniel Gee <dangee1705@gmail.com>
2021-01-25 14:36:03 +0000

    updated the gui to look nice

--- src/gui.py
+++ src/gui.py
@@ -1,8 +1,8 @@
-import flask
+import json
 import time
-
-import ticket_generator
+import flask
 import nlpu
+import ticket_generator
 
 app = flask.Flask(__name__)
 
@@ -18,12 +18,39 @@ def css(stylesheet):
 def js(script):
 	return flask.send_from_directory("templates/js/", script)
 
+@app.route("/img/<img>")
+def img(img):
+	return flask.send_from_directory("templates/img/", img)
+
+@app.route("/favicon.ico")
+def favicon():
+	return flask.send_from_directory("templates/img/", "icon.png")
+
 def i_dont_understand():
 	return "Sorry, I'm not sure I know what you mean. Can you try again?"
+
+def text_message(text):
+	return {"type": "text", "content": text}
+
+def ticket_message(ticket):
+	return {
+		"type": "ticket",
+		"from_station": ticket.from_station,
+		"to_station": ticket.to_station,
+		"leave_at": ticket.leave_at,
+		"arrive_at": ticket.arrive_at,
+		"travel_time": ticket.travel_time,
+		"no_of_changes": ticket.no_changes,
+		"price": ticket.price,
+		"name": ticket.name,
+		"provider": ticket.provider,
+		"link": ticket.link
+	}
 
 @app.route("/message", methods = ["POST"])
 def message():
 	msg = flask.request.get_json()
-	return nlpu.process_message(msg)
+
+	return json.dumps([text_message(nlpu.process_message(msg))])
 
 app.run()
--- src/templates/js/script.js
+++ src/templates/js/script.js
@@ -1,30 +1,47 @@
 var messages_div = document.getElementById("messages");
+var form = document.getElementById("form");
+
 function addMessage(message, side) {
-	var message_p = document.createElement("div");
-	message_p.classList.add(side);
-	message_p.textContent = message;
-	messages_div.append(message_p);
+	var message_div = document.createElement("div");
+	message_div.classList.add("message");
+	message_div.classList.add(side);
+
+	if(message["type"] == "text"){
+		message_div.innerHTML = "<p>" + message["content"] + "</p>";
+	}
+
+	messages_div.append(message_div);
 	messages_div.scrollTop = messages_div.scrollHeight;
 }
 
-var form = document.getElementById("form");
-form.addEventListener("submit", (e) => {
-	e.preventDefault();
+function textMessage(text) {
+	return {
+		"type": "text",
+		"content": text
+	};
+}
 
-	var messagebox = document.getElementById("messagebox");
-	var message = messagebox.value;
-
-	addMessage(message, "right");
-	messagebox.value = "";
-
+function doWork(content) {
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", "/message");
 	xhr.setRequestHeader('Content-Type', 'application/json');
 	xhr.addEventListener("load", (e) => {
-		addMessage(xhr.responseText, "left");
+		var messages = JSON.parse(xhr.responseText);
+		for(var i = 0; i < messages.length; i++)
+			addMessage(messages[i], "left");
 	});
-	xhr.send(JSON.stringify(message));
+	xhr.send(JSON.stringify(content));
+}
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	var messagebox = document.getElementById("messagebox");
+	var content = messagebox.value;
+	doWork(content);
+	addMessage(textMessage(content), "right");
+	messagebox.value = "";
 });
 
-addMessage("Hi, I am trainbot!", "left");
-addMessage("How can I help you?", "left");
+addMessage(textMessage("Hi, I am trainbot!"), "left");
+addMessage(textMessage("How can I help you?"), "left");
